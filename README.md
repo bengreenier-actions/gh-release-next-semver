382acd962fc768134c5b2374d201288994c7741b
bengreenier <ben@bengreenier.com>
2019-11-14 21:40:09 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,37 @@
+# gh-release-next-semver
+
+Github Action that gets the next valid GitHub release semver value
+
+```
+v1.2.3 => 2.0.0
+```
+
+## YAML
+
+```
+- name: Get Next Semver
+  id: next_semver
+  uses: bengreenier-actions/gh-release-next-semver@master
+  with:
+    token: ${{ secrets.GITHUB_TOKEN }}
+    use_tag_name: true
+    strip_tag_prefix: "v"
+    type: "major"
+```
+
+Will yield outputs:
+
+- `next`: contains the next semver value (always with no prefix)
+
+And can be further accessed in yaml with:
+
+```
+- name: Leverage output
+  run: echo ${{ steps.next_semver.outputs.next }}
+```
+
+Where `next_semver` matches the `id` of the step that ran the command.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,66 @@
+const semver = require('semver')
+const core = require('@actions/core')
+const { GitHub, context } = require('@actions/github')
+
+/**
+ * Get configuration from the platform runtime
+ */
+const getConfig = () => ({
+  /**
+   * Github auth token
+   */
+  token: core.getInput('token', { required: true }),
+
+  /**
+   * Should we use the tag name instead of the release name
+   */
+  use_tag_name: core.getInput('use_tag_name', { required: true }),
+
+  /**
+   * A prefix we should strip away
+   */
+  strip_tag_prefix: core.getInput('strip_tag_prefix'),
+
+  /**
+   * Should we bump the major version
+   */
+  type: core.getInput('type', { required: true })
+})
+
+/**
+ * The work our action does, wrapped in an async function
+ */
+const asyncWork = async () => {
+  const config = getConfig()
+  const api = new GitHub(config.token)
+
+  // enumerate releases
+  const releases = await api.repos.listReleases(context.repo)
+  const releaseData = releases.data
+
+  const normalizeToVersion = name => {
+    const v = config.strip_tag_prefix ? name.substr(config.strip_tag_prefix.length) : name
+
+    return semver.parse(v)
+  }
+
+  const top = releaseData
+    .map(a => normalizeToVersion(config.use_tag_name ? a.tag_name : a.name))
+    .sort((a, b) => {
+      return a.compare(b)
+    })[0]
+
+  return top.inc(conf.type).raw
+}
+
+// do the work for the action
+asyncWork().then(
+  output => {
+    // inform the actions runner of our output
+    core.setOutput('next', output)
+  },
+  err => {
+    // inform the actions runner of our failure
+    core.setFailed(err.message)
+  }
+)
